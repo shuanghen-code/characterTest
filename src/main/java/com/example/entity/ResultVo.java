7652dc86af63de46c35d9eca37ebf5d14320f724
root <wk1436233480@163.com>
2021-12-08 15:34:01 +0800

    添加了后台的关于测试人员的性格饼状图 添加人：王凯

---
+++ src/main/java/com/example/entity/ResultVo.java
@@ -0,0 +1,11 @@
+package com.example.entity;
+
+import lombok.Data;
+
+@Data
+public class ResultVo {
+    private Long blueCount;
+    private Long greenCount;
+    private Long yellowCount;
+    private Long redCount;
+}
